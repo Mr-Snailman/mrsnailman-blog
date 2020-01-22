7e065792db09d52f063078c2b443433bb047f2e2
Dustin Saunders <dsaunders2414@gmail.com>
2020-01-21 22:09:33 -0500

    Finished About us content.

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -24,6 +24,9 @@ const styles = (theme) => {
       flexDirection: 'column',
       justifyContent: 'center',
       padding: theme.spacing.unit,
+    },
+    mainTextGridItem: {
+      padding: theme.spacing.unit,
     }
   }
 }
@@ -42,6 +45,12 @@ class LandingContainer extends React.Component {
         <Grid item xs={12} className={classes.columnGridItem}>
           <Typography>Software Engineer, Mentor, Leader</Typography>
           <Typography>Homebrewer, Entrepreneur</Typography>
+        </Grid>
+        <Grid item xs={12} className={ classes.mainTextGridItem }>
+          <Typography paragraph>Truly believing in and valuing the success of others through collaborative hard work and learning, Dustin is a driven and passionate leader that has taken his years in the IT industry to constantly learn, grow, and give back through teaching and mentoring. In his everyday work at Interclypse, Dustin works as a Software Engineer and Technical Leader on several projects. One of his passions is realized at Exerceo, a 501c(3) non-profit, where he strongly advocates for active mentorship in and out of the work place by leading the Software Engineering Program. Interested in everything information technology related, including software engineering, cybersecurity, and system administrator, Dustin is a strong, passionate software engineering candidate.</Typography>
+          <Typography paragraph>Outside of the office, Dustin spends his time as a Homebrewer, practicing and mastering his craft through continual learning and growth through hands-on experience. He is constantly looking for the next big beer to brew, how different ingredients fit into existing recipes and themes, and finally how he can break the traditional and craft mold even further through Extreme Brewing.</Typography>
+          <Typography paragraph>He additionally loves rock climbing (indoor and outdoor), Dungeons and Dragons, and general Science Fiction/Fantasy genres across all media.</Typography>
+          <Typography paragraph>Besides his loving and patient wife, he lives with two rapscallion cats, Chester and Hobbes.</Typography>
         </Grid>
       </React.Fragment>
     )
