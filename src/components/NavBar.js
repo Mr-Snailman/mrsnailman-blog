f0978b07c6d26c2fcc89af6165fd896ac3b5ec66
Dustin Saunders <dsaunders2414@gmail.com>
2020-01-23 22:02:43 -0500

    MenuItems to actual NavBar; removing unused assets.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,7 @@
 import AppBar from '@material-ui/core/AppBar'
-import IconButton from '@material-ui/core/IconButton'
-import Menu from '@material-ui/core/Menu'
-import MenuIcon from '@material-ui/icons/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
 import { makeStyles } from '@material-ui/core/styles'
 import { NavLink } from 'react-router-dom'
-import React, { useState } from 'react'
+import React from 'react'
 import SocialMediaIcons from '../components/SocialMediaIcons'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
@@ -15,21 +11,24 @@ const useStyles = makeStyles((theme) => ({
     zIndex: 1
   },
   title: {
-    flexGrow: 2,
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+  },
+  dungeon: {
     fontFamily: 'Dungeon',
-  }
+    marginRight: theme.spacing(3),
+    textDecoration: 'none',
+  },
 }))
-
-const renderMenuItem = (route, title, handleClose) => (
-  <MenuItem key={ route } component={ NavLink } to={ route } onClick={ handleClose }>{ title }</MenuItem>
-)
 
 export default (props) => {
   const routes = props.routes
-  const menuItems = [
+  const navItems = [
     {
       route: routes.home,
-      title: 'Home',
+      title: 'Thoughtful Brew',
+      props: { variant: 'h4' },
     },
     {
       route: routes.blog,
@@ -46,34 +45,24 @@ export default (props) => {
   ]
 
   const classes = useStyles()
-  const [anchorEl, setAnchorEl] = useState(null)
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
 
   return (
     <AppBar
       position='absolute'
       className={classes.appBar}>
       <Toolbar>
-        <IconButton onClick={ handleClick }><MenuIcon/></IconButton>
-        <Menu
-          id='nav-menu'
-          anchorEl={ anchorEl }
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={ handleClose }>
-          { menuItems.map(el => renderMenuItem(el.route, el.title, handleClose)) }
-        </Menu>
-        <Typography
-          variant='h4'
-          color='inherit'
-          className={classes.title}>
-          Thoughtful Brew
-        </Typography>
+        <div className={ classes.title }>
+          { navItems.map(el =>
+            <Typography
+              className={ classes.dungeon }
+              key={ el.route }
+              color='inherit'
+              variant='h5'
+              component={ NavLink }
+              to={ el.route }
+              { ...el.props }>{ el.title }</Typography>
+          )}
+        </div>
         <SocialMediaIcons/>
       </Toolbar>
     </AppBar>
