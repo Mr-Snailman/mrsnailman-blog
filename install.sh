4160b45f4f9e2dc623c7ef185b0ca74120c48f74
Dustin Saunders <dsaunders2414@gmail.com>
2020-01-30 19:05:20 -0600

    Setup for prod.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -Ga docker $1 
