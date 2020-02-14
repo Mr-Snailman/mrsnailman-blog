cd34eaabecc3b3267e3d1017a73bb7cf8b6f37cd
Dustin Saunders <dsaunders2414@gmail.com>
2020-02-13 22:07:25 -0500

    Using pre-renderer instead of SSR. So much easier.

--- scripts/local-deploy.sh
+++ scripts/local-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 yarn build
+tar zcf build.tar.gz build/
 scp build.tar.gz www.thoughtfulbrew.com:.
 ssh www.thoughtfulbrew.com ./deploy.sh
--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ import ReactGA from 'react-ga'
 import theme from './configureTheme'
 import { ThemeProvider, } from '@material-ui/core/styles'
 
+import { JssProvider } from 'react-jss'
+import { create } from 'jss'
+import preset from 'jss-preset-default'
+
 axios.defaults.withCredentials = true
 
 const config = {
@@ -27,18 +31,29 @@ config.routes = {
 const history = createBrowserHistory()
 const store = configureStore(history, { config: config })
 
+const rootElement = document.getElementById('root')
+
+// See this Issue in react-snap: https://github.com/stereobooster/react-snap/issues/99
+const createGenerateClassName = () => {
+  let counter = 0
+  return (rule, sheet) => `c${Math.random().toString(36).substring(2, 4) + Math.random().toString(36).substring(2, 4)}-${rule.key}-${counter++}`
+}
+const jss = create(preset())
+jss.options.createGenerateClassName = createGenerateClassName
+
 ReactDom.render(
   <Provider store={ store }>
     <ConnectedRouter history={ history }>
-      <ThemeProvider theme={ theme }>
-        <CssBaseline/>
-        <App/>
-      </ThemeProvider>
+      <JssProvider jss={ jss }>
+        <ThemeProvider theme={ theme }>
+          <CssBaseline/>
+          <App/>
+        </ThemeProvider>
+      </JssProvider>
     </ConnectedRouter>
-  </Provider>,
-  document.getElementById('root'))
+  </Provider>, rootElement)
 
-if (process.env.NODE_ENV === 'production') {
+if (navigator.userAgent !== 'ReactSnap' && process.env.NODE_ENV === 'production') {
   ReactGA.initialize('UA-157445631-1')
   ReactGA.pageview(window.location.pathname + window.location.search)
 }
