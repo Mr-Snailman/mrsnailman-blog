00a55f5802e3d1cbc81b1ef24f050ba72ffd21e5
Dustin Saunders <dsaunders2414@gmail.com>
2020-02-18 20:49:56 -0500

    First helmet.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import { Helmet } from 'react-helmet'
 import LayoutContainer from './containers/LayoutContainer'
+import React from 'react'
 import RouteContainer from './containers/RouteContainer'
 
 export default () => {
   return (
     <LayoutContainer>
+      <Helmet>
+        <meta charSet='utf-8'/>
+        <meta name="description" content="Dustin Saunders Blog Website"/>
+        {
+          /* 
+          manifest.json provides metadata used when your web app is installed on a
+          user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+          */
+        }
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json"/>
+        <title>Thoughtful Brew</title>
+      </Helmet>
       <RouteContainer/>
     </LayoutContainer>
   )
