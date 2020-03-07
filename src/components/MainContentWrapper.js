9eb5aee0ef38b7134b4dd0ccde0ef76f7bc263f9
Dustin Saunders <dustin.saunders@sofiac.us>
2020-03-07 17:37:52 -0500

    Contact button on every page.

--- src/components/MainContentWrapper.js
+++ src/components/MainContentWrapper.js
@@ -1,9 +1,17 @@
+import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles(theme => ({
+  contactGrid: {
+    textAlign: 'center',
+    paddingBottom: theme.spacing(2),
+  },
   grid: {
     paddingTop: 64,
     overflow: 'auto',
@@ -25,11 +33,23 @@ const useStyles = makeStyles(theme => ({
 }))
 export default (props) => {
   const classes = useStyles()
+  const contactUsRoute = useSelector(state => state.config.routes.contact)
+  const currentRoute = useSelector(state => state.router.location)
+  const history = useHistory()
   return (
     <Grid container justify='center' className={ classes.grid }>
       <Grid item sm={12} md={10} className={ classes.gridItem }>
         <Paper className={ classes.mainPage }>
           { props.children }
+          { currentRoute.pathname !== '/contact' 
+              ? <Grid container>
+                <Grid item xs={12} className={ classes.contactGrid }>
+                  <Typography>Have a question or a comment?</Typography>
+                  <Button color='primary' variant='outlined' onClick={ () => history.push(contactUsRoute) }>Contact Us</Button>
+                </Grid>
+              </Grid>
+              : null
+          }
         </Paper>
       </Grid>
     </Grid>
--- src/containers/LandingContainer.js
+++ src/containers/LandingContainer.js
@@ -1,23 +1,14 @@
 import blogList from '../blogList'
 import BlogListing from '../components/BlogListing'
-import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
-import Hidden from '@material-ui/core/Hidden'
+import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
 import Seo from '../Seo'
-import SocialMediaIcons from '../components/SocialMediaIcons'
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
-import { useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
 const useStyles = makeStyles(theme => ({
   blogGrid: {
     padding: theme.spacing(2),
-  },
-  contactGrid: {
-    textAlign: 'center',
-    paddingBottom: theme.spacing(2),
   },
   titleGrid: {
     textAlign: 'center',
@@ -29,8 +20,6 @@ const useStyles = makeStyles(theme => ({
 
 export default () => {
   const classes = useStyles()
-  const contactUsRoute = useSelector(state => state.config.routes.contact)
-  const history = useHistory()
   return (
     <React.Fragment>
       <Seo
@@ -53,18 +42,6 @@ export default () => {
       <Grid container className={ classes.blogGrid }>
         <Grid item xs={12}>
           <BlogListing blogList={ blogList.slice(0, 4) }/>
-        </Grid>
-      </Grid>
-      <Grid container justify='center'>
-        <Hidden implementation='css' mdUp>
-          <Grid item xs={12} className={ classes.contactGrid }>
-            <Typography>Connect on Social Media!</Typography>
-          </Grid>
-          <SocialMediaIcons classes={{ gridItem: classes.contactGrid }}/>
-        </Hidden>
-        <Grid item xs={12} className={ classes.contactGrid }>
-          <Typography>Have a question or a comment?</Typography>
-          <Button color='primary' variant='outlined' onClick={ () => history.push(contactUsRoute) }>Contact Us</Button>
         </Grid>
       </Grid>
     </React.Fragment>
