c5406bf5fe7f002ed3fae43e1ebcff449c4f2082
Dustin Saunders <dustin.saunders@sofiac.us>
2020-03-07 17:37:42 -0500

    Removed react-snap.

--- src/index.js
+++ src/index.js
@@ -11,10 +11,6 @@ import ReactGA from 'react-ga'
 import theme from './configureTheme'
 import { ThemeProvider, } from '@material-ui/core/styles'
 
-import { JssProvider } from 'react-jss'
-import { create } from 'jss'
-import preset from 'jss-preset-default'
-
 axios.defaults.withCredentials = true
 
 const config = {
@@ -33,27 +29,17 @@ const store = configureStore(history, { config: config })
 
 const rootElement = document.getElementById('root')
 
-// See this Issue in react-snap: https://github.com/stereobooster/react-snap/issues/99
-const createGenerateClassName = () => {
-  let counter = 0
-  return (rule, sheet) => `c${Math.random().toString(36).substring(2, 4) + Math.random().toString(36).substring(2, 4)}-${rule.key}-${counter++}`
-}
-const jss = create(preset())
-jss.options.createGenerateClassName = createGenerateClassName
-
 ReactDom.render(
   <Provider store={ store }>
     <ConnectedRouter history={ history }>
-      <JssProvider jss={ jss }>
-        <ThemeProvider theme={ theme }>
-          <CssBaseline/>
-          <App/>
-        </ThemeProvider>
-      </JssProvider>
+      <ThemeProvider theme={ theme }>
+        <CssBaseline/>
+        <App/>
+      </ThemeProvider>
     </ConnectedRouter>
   </Provider>, rootElement)
 
-if (navigator.userAgent !== 'ReactSnap' && process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize('UA-157445631-1')
   ReactGA.pageview(window.location.pathname + window.location.search)
 }
