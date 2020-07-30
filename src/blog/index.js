23f86f0a5ebe9f41cd90ef941a30940ffab5dfa9
Dustin Saunders <dustin.saunders@sofiac.us>
2020-07-30 17:40:29 -0400

    Ready for deploy.

--- src/blog/index.js
+++ src/blog/index.js
@@ -2,15 +2,15 @@ import brewingBasics1 from './BrewingBasics1'
 import brewingBasics2 from './BrewingBasics2'
 import brewingBasics3 from './BrewingBasics3'
 import brewingBasics4 from './BrewingBasics4'
-import brewingBasics5 from './BrewingBasics5'
-import brewingBasics6 from './BrewingBasics6'
+//import brewingBasics5 from './BrewingBasics5'
+//import brewingBasics6 from './BrewingBasics6'
 
 export default [
   brewingBasics1,
   brewingBasics2,
   brewingBasics3,
   brewingBasics4,
-  brewingBasics5,
-  brewingBasics6,
+  //brewingBasics5,
+  //brewingBasics6,
 ]
 
