a3ebc62c827ee28a17df82271d067a673fb1e844
Dustin Saunders <dustin.saunders@sofiac.us>
2020-08-13 16:35:51 -0400

    brewing-basics #5.

--- src/blog/index.js
+++ src/blog/index.js
@@ -2,7 +2,7 @@ import brewingBasics1 from './BrewingBasics1'
 import brewingBasics2 from './BrewingBasics2'
 import brewingBasics3 from './BrewingBasics3'
 import brewingBasics4 from './BrewingBasics4'
-//import brewingBasics5 from './BrewingBasics5'
+import brewingBasics5 from './BrewingBasics5'
 //import brewingBasics6 from './BrewingBasics6'
 
 export default [
@@ -10,7 +10,7 @@ export default [
   brewingBasics2,
   brewingBasics3,
   brewingBasics4,
-  //brewingBasics5,
+  brewingBasics5,
   //brewingBasics6,
 ]
 
